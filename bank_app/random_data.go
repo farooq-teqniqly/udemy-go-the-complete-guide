2d334cbe5ab90a4507eba45e508afb54d033e728
farooqfarooq@teqniqly.com <farooq@teqniqly.com>
2024-10-04 10:24:38 -0700

    Refactor GetFullName and GetRandomRunes to return errors
    Updated both GetFullName and GetRandomRunes functions to return errors for invalid inputs. This enhances robustness by ensuring functions handle improper arguments gracefully.

--- bank_app/random_data.go
+++ bank_app/random_data.go
@@ -1,11 +1,22 @@
 package main
 
-import "github.com/Pallinder/go-randomdata"
+import (
+	"errors"
+	"github.com/Pallinder/go-randomdata"
+)
 
-func GetFullName(gender int) string {
-	return randomdata.FullName(gender)
+func GetFullName(gender int) (string, error) {
+	if gender != 0 && gender != 1 {
+		return "", errors.New("gender must be 0 or 1")
+	}
+
+	return randomdata.FullName(gender), nil
 }
 
-func GetRandomRunes(length int) string {
-	return randomdata.RandStringRunes(length)
+func GetRandomRunes(length int) (string, error) {
+	if length < 1 {
+		return "", errors.New("length must be greater than 0")
+	}
+
+	return randomdata.RandStringRunes(length), nil
 }
